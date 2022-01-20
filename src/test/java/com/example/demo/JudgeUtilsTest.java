eceb9bf8942db8a1d6a32c5d16b5733af6d0c398
ohayota <atoy.ryujin.93621339@gmail.com>
2022-01-20 15:34:04 +0900

    Mission2

--- src/test/java/com/example/demo/JudgeUtilsTest.java
+++ src/test/java/com/example/demo/JudgeUtilsTest.java
@@ -188,11 +188,30 @@ class JudgeUtilsTest {
 
 	// ケースNo3
 	// ケース内容：17歳(04/01)でプレイ不可能
-	// 誕生時刻(引数)：2000/04/01
-	// 現在時刻：2017/04/01
-	// 合格条件：false
-	@Test
-	public void testIsRegisterdAge_17歳_プレイ不可() {
-
-	}
+    // 誕生時刻(引数)：2000/04/01
+    // 現在時刻：2017/04/01
+    // 合格条件：false
+    @Test
+    public void testIsRegisterdAge_17歳_プレイ不可() {
+        // 事前処理(Mock)
+        Calendar cal = Calendar.getInstance();
+        cal.clear();
+        cal.set(Calendar.YEAR, 2017);
+        cal.set(Calendar.MONTH, Calendar.APRIL);
+        cal.set(Calendar.DAY_OF_MONTH, 01);
+        Date date = cal.getTime();
+        MockCurrentTime mockTime = new MockCurrentTime(date);
+        // 引数
+        Calendar birthcal = Calendar.getInstance();
+        birthcal.clear();
+        birthcal.set(Calendar.YEAR, 2000);
+        birthcal.set(Calendar.MONTH, Calendar.APRIL);
+        birthcal.set(Calendar.DAY_OF_MONTH, 01);
+        Date birthDay = birthcal.getTime();
+        // 実行
+        boolean actual = target.isRegisterdAge(birthDay);
+        //結果
+        assertFalse(actual);
+        mockTime.tearDown();
+    }
 }
